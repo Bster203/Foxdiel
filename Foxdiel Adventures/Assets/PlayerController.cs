bde637391a30f54365c29c0a16cac7d717c2a2e8
Bster86 <86089590+Bster203@users.noreply.github.com>
2022-08-31 00:29:38 -0400

    chapter 15

--- Foxdiel Adventures/Assets/PlayerController.cs
+++ Foxdiel Adventures/Assets/PlayerController.cs
@@ -4,33 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public Animator anim;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private enum State { idle, running, jumping, falling }
+    private State state = State.idle;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
+        float hDirection = Input.GetAxis("Horizontal");
+
+        if (hDirection < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
-            anim.SetBool("running", true);
         }
 
-        if (Input.GetKey(KeyCode.RightArrow))
+        else if (hDirection > 0)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
-            anim.SetBool("running", true);
         }
 
         else
         {
-            anim.SetBool("running", false);
+
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
             rb.velocity = new Vector2(rb.velocity.x, 10);
+            state = State.jumping;
+        }
+
+        VelocityState();
+        anim.SetInteger("state", (int)state);
+    }
+
+    private void VelocityState()
+    {
+        if (state == State.jumping)
+        {
+
+        }
+
+        else if (Mathf.Abs(rb.velocity.x) > 2f)
+        {
+            //Moving
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
         }
     }
 }
