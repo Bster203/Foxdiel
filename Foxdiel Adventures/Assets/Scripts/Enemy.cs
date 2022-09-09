e526c94f7f7b3d3ce1de0613a4dd0e583e8f5de7
Bster86 <86089590+Bster203@users.noreply.github.com>
2022-09-08 21:44:52 -0400

    Chapter 28

--- Foxdiel Adventures/Assets/Scripts/Enemy.cs
+++ Foxdiel Adventures/Assets/Scripts/Enemy.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     protected Animator anim;
+    protected Rigidbody2D rb;
 
     protected virtual void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void JumpedOn()
--- Foxdiel Adventures/Assets/Scripts/Frog.cs
+++ Foxdiel Adventures/Assets/Scripts/Frog.cs
@@ -11,15 +11,13 @@ public class Frog : Enemy
     [SerializeField] private float jumpHeight = 15f;
     [SerializeField] private LayerMask ground;
     private Collider2D coll;
-    private Rigidbody2D rb;
-
+   
     private bool facingLeft = true;
 
     protected override void Start()
     {
         base.Start();
         coll = GetComponent<Collider2D>();
-        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
